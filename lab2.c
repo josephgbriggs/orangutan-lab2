0b3557b68e03bfb735f833fcd553aeb457b503f7
Joseph Briggs <joseph.g.briggs@acm.org>
2014-03-26 18:34:03 -0500

    renaming framework files

--- lab2.c
+++ lab2.c
@@ -1,48 +1,112 @@
+#define ECHO2LCD
+
 #include <pololu/orangutan.h>
 
+// System tasks
+//#include "menu.h"
+
+//Gives us uintX_t (e.g. uint32_t - unsigned 32 bit int)
+//On the ATMega128 int is actually 16 bits, so it is better to use
+//  the int32_t or int16_t so you know exactly what is going on
+#include <inttypes.h> //gives us uintX_t
+
+// useful stuff from libc
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// GLOBALS
+// tick count for scheduler, yellow task, green task
+volatile uint32_t G_msTicks = 0;
+
+// shared variables with ISRs, including 
+// release flags, task period, toggle counts
+
+int main(void) {
+	// -------------------------------------------------------------
+	// This program teaches you about the various ways to "schedule" tasks.
+	// You can think of the three blinking LEDs as separate tasks with 
+	// strict timing constraints.
+	//
+	// As you build your program, think about the guarantees that you can 
+	// make regarding the meeting of deadlines. Think about how the CPU
+	// is "communicating" with the LEDs. Obviously, everything is output,
+	// but what if it was input? Also think about how data can be passed
+	// among tasks and how conflicts might arise.
+	//
+	// TASKS
+	//
+	// Establish WCET analysis on a for loop to use for timing.
+	// Use the for loop to blink the red LED.
+	//
+	// Set up a system 1 ms software timer (16-bit) and use that to "schedule" 
+	// tasks.
+	//
+	// Toggle the red LED inside a cyclic executive.
+	//
+	// Toggle the yellow LED using a separate timer (8-bit) with a 100ms resolution.
+	// Toggle the LED inside the ISR.
+	//
+	// Toggle the green LED by toggling the output on a pin using
+	// a Compare Match. This is the creation of a PWM signal with a very long period.
+	// Count toggles inside the corresponding ISR (don't forget to turn this
+	// off when the green LED is turned off.
+	//
+	// Scheduler releases the menu task.
+	// The menu allows the user to modify the period of each task.
+	//
+	// --------------------------------------------------------------
+
+	int i;
+
+	// Used to print to serial comm window
+	char tempBuffer[32];
+	int length = 0;
+	
+	// Turn all LEDs on for a second or two then turn off to confirm working properly
+	// Send a message to LCD to confirm proper start. ( interrupts might need to be on for this ?? )
+	// Send a message through serial comm to confirm working properly.
+
+	// Initialize All Tasks
+
+	clear();	// clear the LCD
+
+	//enable interrupts
+	sei();
+	
+	while (1) {
+		/* BEGIN with a simple toggle using for-loops. No interrupt timers */
+
+		// --------- blink LED by using a busy-wait delay implemented with an empty for-loop
 /*
- * Lab 2: for the Orangutan SVP.
- * Joseph G. Briggs
- *
- */
+		LED_TOGGLE(RED);
+		G_redToggles++;
+		length = sprintf( tempBuffer, "R toggles %d\r\n", G_redToggles );
+		print_usb( tempBuffer, length );
 
-int main() {
-  /*  Set up to flash LED at 2 Hz using PWM on Timer1, Channel A (PD5)
-   *     MCU CLOCK    | PRESCALER |   1/TOP   |     Hz
-   * =========================================================
-   * 20,000,000 ticks    1 count      1 int      2 ints
-   * ---------------- *  -------  * --------- = ------- = 2 Hz
-   *       1 sec         N ticks     M counts     1 sec
-   *
-   * N = 256  	--> clk(T1) = clk(i/o)/256
-   * M = 39,062	--> TOP value for pulse wave generation
-   *
-   */
-  // set the clock tick prescaler bits to use clkio/256
-  TCCR1B |= 1 << CS12;
+ #ifdef ECHO2LCD
+		lcd_goto_xy(0,0);
+		printf("R:%d ",G_redToggles);
+#endif
 
-  // Set control register bits for 'mode 14' 
-  // - Fast PWM with ICR1 to define the TOP
-  TCCR1B |= ((1 << WGM13) | (1 << WGM12));
-  TCCR1A |= (1 << WGM11);
+		// create a for-loop to kill approximately 1 second
+		for (i=0;i<100;i++) {
+			WAIT_10MS;
+		}
+*/	
+		// ----------- COMMENT OUT above implementation of toggle and replace with this...
+		// ------------------- Have scheduler release tasks using user-specified period
 
-  // set ICR for TOP value for wave
-  ICR1 = 39062;
 
-  // create the wave, set the comparison to 50% of TOP for a 50% duty cycle
-  OCR1A = 39062 / 2;
 
-  // Set to Clear OC1A on Compare Match, set OC1A at BOTTOM (non-inverting mode)
-  TCCR1A |= 1 << COM1A1;
+		// --------------- RED task
+		
+		// --------------- MENU task
+/*
+		serial_check();
+		check_for_new_bytes_received();
+*/
+					
+	} //end while loop
+} //end main
 
-  // set the data direction register for PortD Pin5 where we are putting our
-  // green LED
-  DDRD |= 1 << DDD5;
-
-  // set the I-flag in the Status Register (interrupts globally enabled),
-  sei();
-
-  while (1) {
-    // no work to do, it is all handled by the timer/clock
-  }
-}
