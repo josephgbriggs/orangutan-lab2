b74e155f738665744fb67acb23f1c8d3e074713e
Joseph Briggs <joseph.g.briggs@acm.org>
2014-03-27 17:24:49 -0500

    file used for analysis of cycles for an empty for loop

--- tasks.h
+++ tasks.h
@@ -10,13 +10,16 @@
 #include "led.h"
 
 /* number of empty for loops to eat up about 10 ms
- * 23 ticks to execute instruction for the empty for-loop
+ *
+ * 33 ticks to execute instruction for the empty for-loop 
+ * according to dissassembled code
+ * 
+ * Calculations below adjusted based on empirical testing
  * 1 sec = 1000 ms / sec 
  * 20,000,000 ticks / 1 sec = 20,000,000 / 1000 ms = 20,000 ticks/ 1ms 
  * 20,000 ticks / ms x 1 loop / 35 ticks =  571.4286 loops / 1 ms = 5714 loops / 10 ms
  */
 
-// #define FOR_COUNT_10MS 8696
 #define FOR_COUNT_10MS 5714
 
 volatile uint32_t __ii;
